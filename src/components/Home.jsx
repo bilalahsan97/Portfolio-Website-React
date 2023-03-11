21b4a472e4fa280f543db923f1f10a9dff7be72d
Bilal Ahsan <ahsan.b97@outlook.com>
2023-03-11 16:35:19 +0000

    Home elements added and stylised.
    Home component stylised and added with introduction.
    View Work button added, stylised and hover fustionality added.
    Social icons menu was clashing with home elements, this bug was fixed.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,26 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
-        
+    <div name='home' className='w-full h-screen bg-[#0a192f] lg:pl-[60px] duration-1000'>
+
+    {/* Container */}  
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <p className='text-pink-600'>Hi, my name is</p>
+      <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Bilal Ahsan</h1>
+      <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm an Aspiring Developer</h2>
+      <p className='text-[#8892b0] py-4 max-w-[700px]'
+      >Currently I am focused on creating responsive web applications using React.
+      </p>
+      <div>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
+        >View Work
+        <span className='group-hover:rotate-90 duration-500'>
+          <HiArrowNarrowRight className='ml-2'/>
+        </span>
+        </button>
+      </div>
+    </div>
+
     </div>
   )
 }
