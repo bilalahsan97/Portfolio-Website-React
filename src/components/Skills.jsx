66c548258331769c5e3a7cb4fa63285a3d8f4e2d
Bilal Ahsan <ahsan.b97@outlook.com>
2023-03-19 23:12:45 +0000

    REV1.5 - WEBSITE UPDATED
    REV 1.5 NOTES
        About section created.
        Skills section created.
        Work section Created.
        Home section breif updated.

---
+++ src/components/Skills.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
+import GitHub from '../assets/github.png'
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 lg:pl-[60px] duration-700'>
+    {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+          <p className='py-4'>// These are the technologies I have worked with</p>
+        </div>
+          {/* logos for technologies */}
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+          
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+            <p className='my-4'>HTML</p>
+          </div>
+
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg} alt="React.js icon" />
+            <p className='my-4'>React</p>
+          </div>
+
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
+            <p className='my-4'>Tailwind</p>
+          </div>
+          
+          <div className='shadow-md  shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt="GitHub icon" />
+            <p className='my-4'>GitHub</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Skills;
